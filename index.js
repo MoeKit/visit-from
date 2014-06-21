66089082c69a808a2116fce3e00629394fdac5c9
airyland <airyland@qq.com>
2014-06-21 20:03:46 +0800

    add todo

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+//@todo we have to get a almost complete url list first
+//@todo weibo&&weixin support
+
 // get user's referred and parse it
 // baidu::http://www.baidu.com/link?url=LtLpHikLBye0sF755pIZYGcc3k3CHHOHWryMKURXroXQvEMQO2NxncA4CTMu_PxSQXVLlXhFVmEXTKsOcegzSK&wd=%E4%BD%A0%E5%A6%B9&issp=1&ie=utf-8&tn=baiduhome_pg&inputT=1345
 // google ip:http://74.125.235.191/url?sa=t&rct=j&q=%E6%92%AD%E7%A7%8D%E7%BD%91&source=web&cd=1&ved=0CCcQFjAA&url=%68%74%74%70%3a%2f%2f%77%77%77%2e%73%65%65%64%69%74%2e%63%6f%6d%2f&ei=ZBdNU6KnI8eSkwXZ4oGoDQ&usg=AFQjCNFylKHywKzhTKFH6GpEzLXxxt3ylQ&bvm=bv.64764171,d.dGI&cad=rjt
